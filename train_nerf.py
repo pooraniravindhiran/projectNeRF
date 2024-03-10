105b15cdb8828650b114caf351d568e74922e37b
Shri Ajay Kumar Ravindhiran <>
2024-03-09 21:40:11 -0700

    Added 3 validations image changes and psnr plot

--- train_nerf.py
+++ train_nerf.py
@@ -38,8 +38,7 @@ lr = 5e-3
 lrate_decay = 250
 
 # Loading Dataset - LLFF and Lego 
-near_thresh, far_thresh, hwf, \
-images, poses, render_poses, i_train, i_val, i_test = load_nerf_dataset(DATASET_TYPE, DATASET_DIR)
+near_thresh, far_thresh, hwf, images, poses, render_poses, i_train, i_val, i_test = load_nerf_dataset(DATASET_TYPE, DATASET_DIR)
     
 # Camera intrinsics
 height, width, focal_length = hwf
@@ -60,13 +59,17 @@ include_input_in_direnc = False
 is_ndc_required = False # set to True only for forward facing scenes
 use_white_bkgd = False # for Lego synthetic data
 use_viewdirs = True
-update_lr_every = 5000
-num_random_rays = 1024 # Random Rays Sampling
+update_lr_every = 0
+num_random_rays = 0 # Random Rays Sampling
 
 num_epochs = 20000
 batch_size = 1 # TODO: why not have more images in batch
 chunk_size = 16384 # because 4096 for 1.2GB of GPU memory
 validate_every = 500
+
+# PSNR plots
+psnr_y = []
+epochs_x = []
 
 model_coarse = NeRF(num_pos_encoding_functions, num_dir_encoding_functions, use_viewdirs).to(device)
 model_fine = NeRF(num_pos_encoding_functions, num_dir_encoding_functions, use_viewdirs).to(device)
@@ -91,6 +94,10 @@ for epoch in tqdm(range(num_epochs)):
     coarse_loss = torch.nn.functional.mse_loss(rgb_coarse, target_img)
     fine_loss = torch.nn.functional.mse_loss(rgb_fine, target_img)
     total_loss = coarse_loss + fine_loss #TODO - why summing it ?
+
+    # PSNR graph
+    psnr_y.append(mse2psnr(total_loss.item()))
+    epochs_x.append(epochs_x)
 
     # Backpropagate
     optimizer.zero_grad()
@@ -122,41 +129,49 @@ for epoch in tqdm(range(num_epochs)):
             os.path.join(LOGDIR, "checkpoint" + str(epoch).zfill(5) + ".tar"),
         )
 
+
     # Evaluation for every few epochs on test pose
     # Validation
     if epoch % validate_every == 0:
         print(f"{epoch} Train loss: {total_loss.item()} Train PSNR : {mse2psnr(total_loss.item())}")
-        val_image_idx = np.random.choice(i_val) # i_val[0]
-        val_img_target = images[val_image_idx].to(device)
-        val_pose = poses[i_val]
-        rgb_val_coarse, rgb_val_fine = run_Nerf(height, width, focal_length, val_pose, use_viewdirs, is_ndc_required,use_white_bkgd,
-            near_thresh, far_thresh, num_coarse_samples_per_ray, num_fine_samples_per_ray,
-            include_input_in_posenc, include_input_in_direnc, num_pos_encoding_functions,
-            num_dir_encoding_functions, model_coarse, model_fine, chunk_size, num_random_rays, mode='eval')
+        plt.plot(epochs_x, psnr_y)
+        plt.title(f"PSNR Graph {epoch}")
+        plt.savefig(f"{LOGDIR}psnr/psnr_{epoch}.png")
+
+        val_image_indices = [111,167,192] # i_val
+
+        for val_image_idx in val_image_indices:
+
+            val_img_target = images[val_image_idx[val_image_idx]].to(device)
+            val_pose = poses[val_image_idx]
+
+            rgb_val_coarse, rgb_val_fine = run_Nerf(height, width, focal_length, val_pose, use_viewdirs, is_ndc_required,use_white_bkgd,
+                near_thresh, far_thresh, num_coarse_samples_per_ray, num_fine_samples_per_ray,
+                include_input_in_posenc, include_input_in_direnc, num_pos_encoding_functions,
+                num_dir_encoding_functions, model_coarse, model_fine, chunk_size, num_random_rays, mode='eval')
+            
+            coarse_loss = torch.nn.functional.mse_loss(rgb_val_coarse, val_img_target)
+            fine_loss = torch.nn.functional.mse_loss(rgb_val_fine, val_img_target)
+            total_loss = coarse_loss + fine_loss 
+
+            rgb_val_fine = rgb_val_fine.reshape(height, width, 3)
+            rgb_val_coarse = rgb_val_coarse.reshape(height, width, 3)
+            print(f"{epoch} {val_image_idx} Val loss: {total_loss.item()} Val PSNR: {mse2psnr(total_loss.item())}")
+            
+            
+            plt.imshow(rgb_val_fine.detach().cpu().numpy())
+            plt.title(f"Fine {epoch}")
+            plt.savefig(f"{LOGDIR}val/val_{val_image_idx}/fine/fine{epoch}.png")
+
+            plt.imshow(val_img_target.detach().cpu().numpy())
+            plt.title(f"Ground truth")
+            plt.savefig(f"{LOGDIR}val/val_{val_image_idx}/gt/gt{epoch}.png")
+
+            plt.imshow(rgb_val_coarse.detach().cpu().numpy())
+            plt.title(f"Coarse {epoch}")
+            plt.savefig(f"{LOGDIR}val/val_{val_image_idx}/coarse/coarse{epoch}.png")
+
+
         
-        coarse_loss = torch.nn.functional.mse_loss(rgb_coarse, target_img)
-        fine_loss = torch.nn.functional.mse_loss(rgb_fine, target_img)
-        total_loss = coarse_loss + fine_loss 
-
-        fig = plt.figure()
-        rgb_val_fine = rgb_val_fine.reshape(height, width, 3)
-        rgb_val_coarse = rgb_val_coarse.reshape(height, width, 3)
-        print(f"{epoch} Val loss: {total_loss.item()} Val PSNR: {mse2psnr(total_loss.item())}")
-        plt.imshow(rgb_val_fine.detach().cpu().numpy())
-        plt.title(f"Iteration {epoch}")
-        plt.savefig(f"{LOGDIR}val/val_{val_image_idx}_{epoch}.png")
-
-        plt.figure(figsize=(25, 4))
-        plt.subplot(131)
-        plt.imshow(val_img_target.detach().cpu().numpy())
-        plt.title(f"Ground truth")
-        plt.subplot(132)
-        plt.imshow(rgb_val_coarse.detach().cpu().numpy())
-        plt.title("Coarse")
-        plt.subplot(133)
-        plt.imshow(rgb_val_fine.detach().cpu().numpy())
-        plt.title("Fine")
-        plt.savefig(f"{LOGDIR}val_comp/val_{val_image_idx}_{epoch}.png")
-
-        
-        # plt.show()
+            # plt.show()
+        # Validation done
